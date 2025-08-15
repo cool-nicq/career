def06891615fd1542e9c97b60707b64af901c91b
Ratan Thapa <mail@ratan.com.np>
2025-08-15 13:31:23 +0545

    favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <title>Vacancies | IGI Prudential Insurance</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    
+    <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
     <style>
         :root {
             --primary-color: #e53935; /* A shade of red */
